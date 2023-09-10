a93e9b700411154ca6e039f84e610186eb704e46
Комлев Данила <komlevdanila742@gmail.com>
2023-09-10 10:00:49 +0300

    spark

--- v1/api.py
+++ v1/api.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.sql import SparkSession
 
 router = APIRouter()
+
+spark = SparkSession.builder \
+    .master('local[*]') \
+    .config("spark.driver.memory", "5g") \
+    .config("spark.dynamicAllocation.minExecutors", "4")\
+    .appName('Recommender_system') \
+    .getOrCreate()
+
+save_path = "C:\\Хакатоны\ЦП_НН\\"
+
+model = ALSModel.load(f'{save_path}saved_model')
 
 
 @router.get("/get_prediction")
 async def get_prediction(user_id):
-    return {"message": "Hello World"}
+    return model.recommendForUserSubset([user_id], 10)
